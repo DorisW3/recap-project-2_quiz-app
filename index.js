7f2dbf389922d85a5920f2c2f730e170fcb78938
Doris Wiebe <doris-wiebe@gmx.de>
2023-07-06 11:16:03 +0200

    bookmarkButtonToggle

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // phew… not a lot going on here. Please add some code!
+console.clear();
+
+const button = document.querySelector('[data-js="button"]');
+const buttonSvg = document.querySelector('[data-js="button-svg"]');
+
+button.addEventListener("click", () => {
+  buttonSvg.classList.toggle("bookmark--active");
+  buttonSvg.classList.toggle("bookmark");
+});
+
+
