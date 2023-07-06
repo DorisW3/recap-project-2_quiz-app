4e357dd9eefa27b1080cec931d46b0e16bf5dd50
Doris Wiebe <doris-wiebe@gmx.de>
2023-07-06 12:23:45 +0200

    show-and-hide-answer

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // phew… not a lot going on here. Please add some code!
 console.clear();
 
+// bookmark-toggle:
+
 const button = document.querySelector('[data-js="button"]');
 const buttonSvg = document.querySelector('[data-js="button-svg"]');
 
@@ -9,4 +11,18 @@ button.addEventListener("click", () => {
   buttonSvg.classList.toggle("bookmark");
 });
 
+// show/hide answer:
 
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const showAnswer = document.querySelector('[data-js="answer"]');
+
+answerButton.addEventListener("click", () => {
+  showAnswer.classList.toggle("card__answer--active");
+  showAnswer.classList.toggle("card__answer--hidden");
+
+  if (showAnswer.classList.contains("card__answer--active")) {
+    answerButton.textContent = "Hide answer";
+  } else {
+    answerButton.textContent = "Show answer";
+  }
+});
